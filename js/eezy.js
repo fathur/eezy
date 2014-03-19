00a6d7be229d7bff13809dfff02ac672e1b53a4f
Fathur Rohman <fathur.rohman@fostrom.com>
2014-03-19 18:43:52 +0700

    UPDATE CODE TERBARU DAN LEBIH RAPIH, HEHEHE

--- js/eezy.js
+++ js/eezy.js
@@ -3,23 +3,17 @@ $(".nav li a[href^='#']").on('click', function(e) {
 	e.preventDefault();
 	// store hash
 	var hash = this.hash;
-	console.log(hash);
-	var offset = $(this.hash).offset().top;
-	
-	if (hash == "#about") {
+	var offset = $(this.hash).offset().top;	
+	if($(window).width() < 767) {
+		offset -= 58;
+	} else {
 		offset -= 76;
-	} 
-	// animate
+	}	
 	$('html, body').animate({
 		scrollTop: offset
 	}, 1000);
-	
-	
-	
-	
 	$('.nav li').removeClass('active');
 	$(this).parent().addClass('active');
-	//console.log();
 });
 
 $(".navbar-brand[href^='#']").on('click', function(e) {
@@ -27,27 +21,11 @@ $(".navbar-brand[href^='#']").on('click', function(e) {
 	e.preventDefault();
 	// store hash
 	var hash = this.hash;
-	console.log(hash);
 	var offset = $(this.hash).offset().top;
-	
-	
 	// animate
 	$('html, body').animate({
 		scrollTop: offset-100
 	}, 1000);
-	
-	
-	
-	
 	$('.nav li').removeClass('active');
 	$(this).parent().addClass('active');
-	//console.log();
 });
-/*
-var offset = 45;
-
-$('.nav li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -offset);
-});*/
