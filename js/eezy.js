d9f78c1aa61ed60ceec1bf87041cb82e196acb9f
fathur <fathur_rohman17@yahoo.co.id>
2014-03-20 08:00:11 +0700

    slider 2 dan 3 sudah autoheight, mencari height yang paling tinggi

--- js/eezy.js
+++ js/eezy.js
@@ -29,3 +29,57 @@ $(".navbar-brand[href^='#']").on('click', function(e) {
 	$('.nav li').removeClass('active');
 	$(this).parent().addClass('active');
 });
+
+function carouselNormalization() {
+	var items = $('#ez-six .item'), //grab all slides
+		heights = [], //create empty array to store height values
+		tallest; //create variable to make note of the tallest slide
+
+	if (items.length) {
+		function normalizeHeights() {
+			items.each(function() { //add heights to array
+				heights.push($(this).height()); 
+			});
+			tallest = Math.max.apply(null, heights); //cache largest value
+			items.each(function() {
+				$(this).css('min-height',tallest + 'px');
+			});
+		};
+		normalizeHeights();
+
+		$(window).on('resize orientationchange', function () {
+			tallest = 0, heights.length = 0; //reset vars
+			items.each(function() {
+				$(this).css('min-height','0'); //reset min-height
+			}); 
+			normalizeHeights(); //run it again 
+		});
+	}
+}
+
+function carouselNormalizationSeven() {
+	var itemsSeven = $('#ez-seven .item'), //grab all slides
+		heightsSeven = [], //create empty array to store height values
+		tallestSeven; //create variable to make note of the tallest slide
+
+	if (itemsSeven.length) {
+		function normalizeHeightsSeven() {
+			itemsSeven.each(function() { //add heights to array
+				heightsSeven.push($(this).height()); 
+			});
+			tallestSeven = Math.max.apply(null, heightsSeven); //cache largest value
+			itemsSeven.each(function() {
+				$(this).css('min-height',tallestSeven + 'px');
+			});
+		};
+		normalizeHeightsSeven();
+
+		$(window).on('resize orientationchange', function () {
+			tallestSeven = 0, heightsSeven.length = 0; //reset vars
+			itemsSeven.each(function() {
+				$(this).css('min-height','0'); //reset min-height
+			}); 
+			normalizeHeightsSeven(); //run it again 
+		});
+	}
+}
