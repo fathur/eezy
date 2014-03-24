042900d9ce7707a9dddf53eeca226760815e0162
Fathur Rohman <fathur.rohman@fostrom.com>
2014-03-24 09:16:34 +0700

    just update

--- js/eezy.js
+++ js/eezy.js
@@ -1,33 +1,4 @@
-$(".nav li a[href^='#']").on('click', function(e) {
-	// prevent default anchor click behavior
-	e.preventDefault();
-	// store hash
-	var hash = this.hash;
-	var offset = $(this.hash).offset().top;	
-	if($(window).width() < 767) {
-		offset -= 58;
-	} else {
-		offset -= 76;
-	}	
-	$('html, body').animate({
-		scrollTop: offset
-	}, 1000);
-	$('.nav li').removeClass('active');
-	$(this).parent().addClass('active');
-});
-$(".navbar-brand[href^='#']").on('click', function(e) {
-	// prevent default anchor click behavior
-	e.preventDefault();
-	// store hash
-	var hash = this.hash;
-	var offset = $(this.hash).offset().top;
-	// animate
-	$('html, body').animate({
-		scrollTop: offset-100
-	}, 1000);
-	$('.nav li').removeClass('active');
-	$(this).parent().addClass('active');
-});
+
 
 function detectmob() { 
 	if( navigator.userAgent.match(/Android/i)
@@ -84,9 +55,6 @@ function normalizeHeights(items,heights,tallest) {
 		heights.push(height);		
 	});
 	tallest = Math.max.apply(null, heights);
-	
-	console.log(heights);
-	console.log(tallest);
 	
 	items.each(function(){
 		$(this).css('height', tallest + 60 + 'px');
